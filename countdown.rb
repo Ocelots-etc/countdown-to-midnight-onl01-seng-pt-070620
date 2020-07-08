73fc823c9478de392424998e19509fac40981ceb
Cat Gomez <catherinedgomez@gmail.com>
2020-07-08 22:39:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(number = 10)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n = 5, s = 2)
+  while n > 0
+    puts "sleeping..."
+    sleep(2)
+    n -= 1
+  end
 end
